136e9b983e7bf6c69f51cef3b30c8719503b31c7
Rohit ranjan <ranjanrohit812@gmail.com>
2020-09-11 17:36:59 +0530

    Array List

---
+++ src/com/company/rohit/session3/ArrayListIntro.java
@@ -0,0 +1,21 @@
+package com.company.rohit.session3;
+
+import java.util.ArrayList;
+
+public class ArrayListIntro {
+    public static void main(String[] args) {
+//        ArrayList<Integer> list = new ArrayList<>();
+//        for (int i = 0; i < 5; i++) {
+//            list.add(i);
+//        }
+//        System.out.println(list);
+//        Collections.sort(list);
+
+        ArrayList list = new ArrayList();
+        list.add(5);
+        list.add("Char");
+        list.add('c');
+        System.out.println(list);
+
+    }
+}
